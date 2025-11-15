dc7bbabf0746c2bac13ca7100bddd329b505644f
jackinthebox314 <jwangjy315@gmail.com>
2025-11-15 02:10:18 -0500

    begin work on scraping

--- scrape/get_urls.py
+++ scrape/get_urls.py
@@ -2,13 +2,15 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
-base_url = "https://bulletins.nyu.edu"
-programs_url = base_url + "/undergraduate/arts-science/#programstext"
+programs_url = "https://bulletins.nyu.edu/undergraduate/arts-science/#programstext"
 soup = BeautifulSoup(requests.get(programs_url, headers={'User-Agent': "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/117.0"}).content, 'html.parser')
 
 sitemap = soup.find("div", class_="sitemap")
 programs = sitemap.find_all("a")
-programs_info = [{"name": a.get_text(strip=True), "href": a.get("href"), "visited": False} for a in programs]
+programs_info = [{"name": a.get_text(strip=True),
+                  "href": a.get("href"),
+                  "visited": False}
+                for a in programs]
 
 df = pd.DataFrame.from_dict(programs_info)
 df.to_csv("program_urls.csv", index=False)
--- scrape/scrape.py
+++ scrape/scrape.py
@@ -1,26 +1,37 @@
 from bs4 import BeautifulSoup
-from selenium import webdriver
 from tqdm import tqdm
+import pandas as pd
 import requests
+from pprint import pprint
 
 base_url = "https://bulletins.nyu.edu"
-programs_url = base_url + "/undergraduate/arts-science/#programstext"
-soup = BeautifulSoup(requests.get(programs_url, headers={'User-Agent': "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/117.0"}).content, 'html.parser')
 
-sitemap = soup.find("div", class_="sitemap")
-programs = sitemap.find_all("a")
-programs_info = [{"name": a.get_text(strip=True), "href": a.get("href"), "visited": False} for a in programs]
+programs = pd.read_csv("program_urls.csv")[:1]
 
-print(programs_info)
-
-driver = webdriver.Chrome()
-
-for program in programs_info:
-    if program['visited']:
+for idx, program in tqdm(programs.iterrows(), total=len(programs)):
+    if program['visited'] == "True":
         continue
-    url = base_url + program['href']
-    driver.get(url)
+    url = base_url + program['href'] + "/#curriculumtext"
+    headers = {
+    "User-Agent": (
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/120.0.0.0 Safari/537.36"
+        )
+    }
+    html = requests.get(url, headers=headers).content
+    soup = BeautifulSoup(html, "html.parser")
+    
+    courselist = soup.find("table", class_="sc_courselist")
+    headers = [th.get_text(strip=True) for th in courselist.find_all("th")]
+    
+    courselist_rows = courselist.find("tbody").find_all("tr")[1:] # skip first row (which is "Minor Requirements")
+    pprint(courselist_rows)
+    
+    
     """
     uncomment below when fully implemented
     """
     # program['visited'] = True
+    
+programs.to_csv("program_urls.csv", index=False)
